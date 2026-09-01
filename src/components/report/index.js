b15a36a77c491bc5deefe4992dc6783adbdb5149
Ayushi Patel <ayuship@vconstruct.in>
2026-09-01 12:20:17 +0530

    code refactor

---
+++ src/components/report/index.js
@@ -0,0 +1,15 @@
+import ReportColumnSelector from './ReportColumnSelector';
+import ReportExportButton from './ReportExportButton';
+import ReportFilters from './ReportFilters';
+import ReportPagination from './ReportPagination';
+import ReportSearch from './ReportSearch';
+import ReportTable from './ReportTable';
+
+export {
+  ReportColumnSelector,
+  ReportExportButton,
+  ReportFilters,
+  ReportPagination,
+  ReportSearch,
+  ReportTable
+};
--- src/features/UserForm/UserForm.jsx
+++ src/features/UserForm/UserForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { TextField, Button, Box } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
